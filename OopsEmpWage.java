063171e7d37a82c590d635cc8bb5cbdc48816d25
Sainath470 <akulavenkatasainath1997@gmail.com>
2021-03-25 00:00:47 +0530

    Added:Full time work and wage per hour

--- OopsEmpWage.java
+++ OopsEmpWage.java
@@ -2,31 +2,41 @@ import java.util.*;
 
 public class OopsEmpWage
 {
+		//initializing variables using static so that they can use
+		// any where in class or inside method
+		static int TotalWage=0;
+		static int WagePerHr=20;
+		static int Full_time=8;
 
 		public static int attendance()
 		{
-				Random emp = new Random();
-				int attend=emp.nextInt(2);
-				return attend;
+		Random emp = new Random();
+		int attend=emp.nextInt(2);
+		return attend;
 		}
 
-
-		public static void EmpCheck()
+		//method to check whether employee is present or not
+		public void EmpCheck()
 		{
 			int check=attendance();
 			if(check==0)
 			{
 				System.out.println("Employee is Present");
+				TotalWage+=Full_time*WagePerHr;
+				System.out.println("Total Wage is "+TotalWage);
 			}
 			else
 			{
-				System.out.println("EMployee is Absent");
+				System.out.println("Employee is Absent");
 			}
 		}
 
-
 		public static void main(String args[])
 		{
-			EmpCheck();
+			OopsEmpWage emp1=new OopsEmpWage();
+			emp1.EmpCheck();//calling EmpCheck emthod using object
 		}
+
 }
+
+
