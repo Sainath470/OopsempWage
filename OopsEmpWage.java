5c3df468569a7030d07cf02331f8d2a90e61f8f3
Sainath470 <akulavenkatasainath1997@gmail.com>
2021-03-24 23:22:57 +0530

    Added: Employee check

--- OopsEmpWage.java
+++ OopsEmpWage.java
@@ -1,1 +1,32 @@
-"Wlcome to Employee Wage Computation using Oops concept"
+import java.util.*;
+
+public class OopsEmpWage
+{
+
+		public static int attendance()
+		{
+				Random emp = new Random();
+				int attend=emp.nextInt(2);
+				return attend;
+		}
+
+
+		public static void EmpCheck()
+		{
+			int check=attendance();
+			if(check==0)
+			{
+				System.out.println("Employee is Present");
+			}
+			else
+			{
+				System.out.println("EMployee is Absent");
+			}
+		}
+
+
+		public static void main(String args[])
+		{
+			EmpCheck();
+		}
+}
